b0fcaf3a82e117a83105190cf3806a5363105d30
Gailoks <yra.cimonov@gmail.com>
2025-06-01 13:54:54 +0300

    version v2

--- README.md
+++ README.md
@@ -2,3 +2,35 @@ To build this you should run
 ```bash
 docker build -t ghcr.io/games-on-whales/kde:custom --build-arg BASE_APP_IMAGE=ghcr.io/games-on-whales/base-app:edge .
 ```
+
+Note
+There is a bug with mouse in fullscreen x11 apps. Press right ctrl to fix it. And again if you return to desktop
+
+
+# Wolf config toml
+
+```toml
+[[apps]]
+title = 'Desktop (kde)'
+
+[apps.runner]
+type = 'docker'
+name = 'WolfKDE'
+image = 'ghcr.io/games-on-whales/kde:edge'
+env = ['GOW_REQUIRED_DEVICES=/dev/input/* /dev/dri/* /dev/nvidia*']
+devices = []
+mounts = []
+ports = []
+base_create_json = """
+{
+  "HostConfig": {
+    "IpcMode": "host",
+    "Privileged": false,
+    "CapAdd": ["SYS_ADMIN", "SYS_NICE", "SYS_PTRACE", "NET_RAW", "MKNOD", "NET_ADMIN"],
+    "SecurityOpt": ["seccomp=unconfined", "apparmor=unconfined"],
+    "DeviceCgroupRules": ["c 13:* rmw", "c 244:* rmw"]
+  }
+}
+\
+"""
+```
--- kde/build/scripts/startup.sh
+++ kde/build/scripts/startup.sh
@@ -16,20 +16,40 @@ mkdir -p /tmp/.X11-unix/
 chown retro:retro /tmp/.X11-unix/
 chmod +t /tmp/.X11-unix
 
+# Make user directory
+mkdir -p ~/Desktop ~/Documents ~/Downloads ~/Music ~/Pictures ~/Public ~/Templates ~/Videos
+chmod 755 ~/Desktop ~/Documents ~/Downloads ~/Music ~/Pictures ~/Public ~/Templates ~/Videos
+
 # Flatpak
-export $(dbus-launch)
 export XDG_DATA_DIRS=/var/lib/flatpak/exports/share:/home/retro/.local/share/flatpak/exports/share:/usr/local/share/:/usr/share/
-flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+#flatpak override --user --nosocket=wayland
+sudo /opt/gow/startdbus
+export $(dbus-launch)
+
+# Heroic symlync to shared storage
+[ -d $HOME/Games ] || ln -s /shared /home/retro/Games
+
+
+if [ ! -f $HOME/.config/kwinrc ]; then
+	kwriteconfig5 --file $HOME/.config/kwinrc --group Compositing --key Enabled enable
+fi
+if [ ! -f $HOME/.config/kscreenlockerrc ]; then
+	kwriteconfig5 --file $HOME/.config/kscreenlockerrc --group Daemon --key Autolock false
+fi
 
 export XDG_CURRENT_DESKTOP=KDE
 export DE=kde
 export DESKTOP_SESSION=kde
 export XDG_SESSION_TYPE=wayland
-# Disable compositing and screen lock
-if [ ! -f $HOME/.config/kwinrc ]; then
-	kwriteconfig6 --file $HOME/.config/kwinrc --group Compositing --key Enabled true
-fi
-if [ ! -f $HOME/.config/kscreenlockerrc ]; then
-	kwriteconfig6 --file $HOME/.config/kscreenlockerrc --group Daemon --key Autolock false
-fi
-launcher startplasma-wayland --xwayland
+launcher startplasma-wayland --xwyaland
+# Check eula
+#[ -f $HOME/EULA ] || echo eula=false > $HOME/EULA
+#if grep -Fxq "eula=true" $HOME/EULA
+#then
+#    gow_log "EULA accepted"
+#    startplasma-wayland --xwyaland
+#else
+#    gow_log "EULA regected"
+#    firefox
+#fi
